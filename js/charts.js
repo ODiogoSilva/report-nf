cdfefca7d3d53b33576079f4418ccb8b54458d5b
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-24 10:49:23 +0000

    Added multicolumn header

--- js/charts.js
+++ js/charts.js
@@ -4,9 +4,34 @@ const app = angular.module("reportsApp", []);
 app.controller("reportsController", function($scope, $http){
 
     $scope.workflow_name = "INNUca";
-    $scope.table_headers = ["ID", "Sample name", "reads", "bp", "coverage 1",
-                            "trimmed", "coverage 2", "contigs", "assembled bp",
-                            "contigs 2", "assembled bp 2"];
+    $scope.table_headers_up = [
+        ["ID", "2", "1"],
+        ["Sample name", "2", "1"],
+        ["reads", "2", "1"],
+        ["bp", "2", "1"],
+        ["coverage 1", "2", "1"],
+        ["trimmed", "2", "1"],
+        ["coverage 2", "2", "1"],
+        ["spades", "1", "2"],
+        ["pilon", "1", "2"]
+    ];
+    $scope.table_headers_down = [
+        "contigs", "assembled bp",
+        "contigs 2", "assembled bp 2"
+    ];
+    $scope.table_footer = [
+        "ID",
+        "Sample name",
+        "reads",
+        "bp",
+        "coverage 1",
+        "trimmed",
+        "coverage 2",
+        "contigs",
+        "assembled bp",
+        "contigs 2",
+        "assembled bp 2"
+    ];
     
     $scope.graph1_name = "Graph 1";
     $scope.graph2_name = "Graph 2";
