80df2e69284ae6df1d2944fa6109e5172bfb9896
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-24 14:42:03 +0000

    create reports.js file

---
+++ js/reports.js
@@ -0,0 +1,37 @@
+const app = angular.module("reportsApp", []);
+
+app.controller("reportsController", function($scope, $http){
+
+    init_table($scope);
+
+    get_reports_by_project(1).then((results) => {
+        build_table(results);
+        build_charts(results)
+
+        $("#waiting_gif").css({display:"none"});
+        $("#body_container").css({display:"block"});
+    });
+
+    $('.toggle_sidebar').on("click", () => {
+        /* to toggle the sidebar, just switch the CSS classes */
+        $("#workflows_sidebar").toggleClass("collapsed_sidebar");
+        $("#workflows_content").toggleClass("col-md-12 col-md-8");
+        $(".toggle_sidebar").toggleClass("hide_button");
+        return false;
+    });
+
+    $scope.switch_workflow = (workflow_name) => {
+        console.log("Workflow change");
+        $scope.workflow_name = workflow_name;
+    };
+
+    $('.selectpicker').selectpicker({
+      style: 'btn-info',
+      size: 4
+    });
+
+    $("#waiting_gif").css({display:"none"});
+    $("#body_container").css({display:"block"});
+
+
+});
