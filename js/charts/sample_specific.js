36a2b45186a856e9cc10497384de194afa68b17f
ODiogoSilva <o.diogosilva@gmail.com>
2017-12-15 10:31:18 +0000

    Added missing function

--- js/charts/sample_specific.js
+++ js/charts/sample_specific.js
@@ -113,6 +113,31 @@ const sparkline = (sample, color) => {
 
 };
 
+const updateLabels = (el, fw, idx) => {
+
+    const AxisStyle = {
+        title: {
+            style: {
+                fontWeight: fw,
+            }
+        },
+    };
+
+    let AxisArray;
+
+    if ( idx === 0 ) {
+        AxisArray = [{}, AxisStyle];
+    } else {
+        AxisArray = [AxisStyle, {}];
+    }
+
+    el.chart.update({
+        yAxis: AxisArray,
+        xAxis: AxisArray
+    })
+};
+
+
 /**
  *
  * @param el
