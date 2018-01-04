06f285d976753f867d7272e9ea6e3e84317adf81
ODiogoSilva <o.diogosilva@gmail.com>
2018-01-04 11:30:01 +0000

    Moved sidebar init to navbar.js

--- js/inits.js
+++ js/inits.js
@@ -385,34 +385,3 @@ const initResubmit = (scope) => {
 
     })
 };
-
-
-/**
- * Initialize the toggle behaviour for the sidebar, bound to the menu button
- * in the navbar
- */
-const initToggleSidebar = () => {
-
-    // when opening the sidebar
-    $("#sidebar-button").on("click", () => {
-
-        const sidebarSel = $("#sidebar");
-        const sidebarBtn = $("#sidebar-button");
-
-        if (sidebarSel.hasClass("active")) {
-            sidebarBtn.css({color: "#ffffff"});
-            $(".overlay").fadeOut();
-            $(".popover").removeClass("in");
-        } else {
-            sidebarBtn.css({color: "#28a745"});
-            // fade in the overlay
-            $(".overlay").fadeIn();
-            $("a[aria-expanded=true]").attr("aria-expanded", "false");
-        }
-
-        // open sidebar
-        sidebarSel.toggleClass("active");
-
-    });
-
-};
