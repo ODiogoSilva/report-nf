cd213360edb854e8e4a7649922f49f8b1e02a8f4
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-05 13:39:04 +0000

    add user data

--- js/iframe_functions.js
+++ js/iframe_functions.js
@@ -1,6 +1,8 @@
 const runFromParent = () => {
-    console.log("AQUI");
-    console.log(window.parent);
     window.parent.setUpFrame();
     return true;
+};
+
+const addUserData = (username, userID) => {
+    console.log("InFrame", username, userID);
 };
